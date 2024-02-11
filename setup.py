db516691b4eee9191c1bb4ad53e575678a884636
Thewildweb <erik@thewildweb.nl>
2024-02-11 13:14:32 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Attribute Parser",
+    version="0.1",
+    packages=find_packages(exclude=["tests*"]),
+    license="MIT",
+    description="Some way to parse attributes.",
+    long_description=open("README.md").read(),
+    url="https://github.com/Thewildweb/attribute_parser.git",
+    author="Erik Meijer",
+    author_email="erik@datadepartment.io",
+)
