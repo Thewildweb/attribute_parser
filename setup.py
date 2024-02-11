81422924879a4e0189d5cb7735b1a209ab4e708d
Thewildweb <erik@thewildweb.nl>
2024-02-11 14:53:51 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     license="MIT",
     description="Some way to parse attributes.",
-    long_description=open("README.md").read(),
+    long_description=open("readme.md").read(),
     url="https://github.com/Thewildweb/attribute_parser.git",
     author="Erik Meijer",
     author_email="erik@datadepartment.io",
